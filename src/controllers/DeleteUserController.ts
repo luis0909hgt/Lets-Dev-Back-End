c04426bd105677973dff6d75cbe2084737167b59
luis0909hgt <luishenriquegt@gmail.com>
2022-07-27 21:22:32 -0300

    feat -> aula 07 finalizada

--- src/controllers/DeleteUserController.ts
+++ src/controllers/DeleteUserController.ts
@@ -3,11 +3,14 @@ import { DeleteUserService } from "../services/DeleteUserService";
 
 class DeleteUserController {
     async control(request: Request, response: Response): Promise<Response> {
-        const { id } = request.user;
+        const id = request.headers["x-users-id"] as string || 
+        request.user.id as string;
+
+        const convertUsersIdToArray = id.split(", ");
 
         const deleteUserService = new DeleteUserService();
 
-        await deleteUserService.execute({ id });
+        await deleteUserService.execute({ id: convertUsersIdToArray });
 
         return response.status(204).send();
     }
--- src/services/DeleteUserService.ts
+++ src/services/DeleteUserService.ts
@@ -2,19 +2,27 @@ import { UsersRepository } from "../database/repositories/UsersRepository";
 import { AppError } from "../shared/errors";
 
 type TDeleteUser = {
-    id: string;
+    id: string[];
 }
 
 class DeleteUserService {
+    private count: number = 0;
     async execute({ id }: TDeleteUser): Promise<void> {
+        const actualId = id[0];
         const usersRepository = new UsersRepository();
 
-        const usersAlreadyExists = await usersRepository.findById({ id });
+        const usersAlreadyExists = await usersRepository.findById({ id: actualId });
         if(!usersAlreadyExists) {
             throw new AppError("User not found", 404);
         }
 
-        await usersRepository.delete({ id });
+        await usersRepository.delete({ id: actualId });
+        console.log(this.count++);
+
+        id.shift();
+        if(id && id.length > 0) {
+            await this.execute({ id })
+        }
     }
 }
 
